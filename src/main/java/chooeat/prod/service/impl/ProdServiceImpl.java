5160bff1cec10965d4ee52c6f5a5e6a18ebe11a4
Joanne <jann84920@gmail.com>
2023-06-05 11:50:59 +0800

    商城0605更新評論功能

--- src/main/java/chooeat/prod/service/impl/ProdServiceImpl.java
+++ src/main/java/chooeat/prod/service/impl/ProdServiceImpl.java
@@ -1,16 +1,24 @@
 package chooeat.prod.service.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import chooeat.prod.model.vo.Prod;
 import chooeat.prod.service.ProdService;
 @Service
 public class ProdServiceImpl implements ProdService {
-
+	@Autowired
+	private DataSource dataSource;
 	public List<Prod> selectAll() {
 		return null;
 	}
@@ -56,4 +64,60 @@ public class ProdServiceImpl implements ProdService {
 			Collections.sort(prod, Comparator.comparing(Prod::getProdPrice));
 		}
 	}
+	
+	
+	public List<Prod> getByCategory(String category) {
+	    String sql = "SELECT prod_id,"
+	            + "r.restaurant_id,"
+	            + "prod_name,"
+	            + "prod_text,"
+	            + "prod_userguide,"
+	            + "prod_price,prod_qty,"
+	            + "prod_state,prod_pic,"
+	            + "prod_comment_number,"
+	            + "prod_comment_score,"
+	            + "res_acc,"
+	            + "res_name,"
+	            + "res_add,"
+	            + "GROUP_CONCAT(res_type_name SEPARATOR ' / ') AS category_names "
+	            + "FROM "
+	            + "((chooeat.prod p JOIN chooeat.RESTAURANT r ON p.RESTAURANT_ID = r.RESTAURANT_ID) "
+	            + "JOIN chooeat.RES_TYPE_DETAIL rtd ON p.RESTAURANT_ID = rtd.RESTAURANT_ID) "
+	            + "JOIN chooeat.RES_TYPE rt ON rtd.RES_TYPE_ID = rt.RES_TYPE_ID "
+	            + "GROUP BY p.prod_id, r.RESTAURANT_ID, res_name "
+	            + "HAVING category_names LIKE ?";
+
+	    try (
+	        Connection conn = dataSource.getConnection();
+	        PreparedStatement pstmt = conn.prepareStatement(sql);
+	    ) {
+	        pstmt.setString(1, "%" + category + "%");
+	        ResultSet rs = pstmt.executeQuery();
+	        List<Prod> list = new ArrayList<Prod>();
+	        while (rs.next()) {
+	            Prod prod = new Prod();
+	            prod.setProdId(rs.getInt("PROD_ID"));
+	            prod.setRestaurantId(rs.getInt("RESTAURANT_ID"));
+	            prod.setProdName(rs.getString("PROD_NAME"));
+	            prod.setProdText(rs.getString("PROD_TEXT"));
+	            prod.setProdUserguide(rs.getString("PROD_USERGUIDE"));
+	            prod.setProdPrice(rs.getInt("PROD_PRICE"));
+	            prod.setProdQty(rs.getInt("PROD_QTY"));
+	            prod.setProdState(rs.getInt("PROD_STATE"));
+	            prod.setProdPic(rs.getByte("PROD_PIC"));
+	            prod.setProdCommentNumber(rs.getInt("PROD_COMMENT_NUMBER"));
+	            prod.setProdCommentScore(rs.getInt("PROD_COMMENT_SCORE"));
+	            prod.setResName(rs.getString("RES_NAME"));
+	            prod.setResAdd(rs.getString("RES_ADD"));
+	            prod.setResType(rs.getString("category_names"));
+	            list.add(prod);
+	        }
+	        return list;
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	    return null;
+	}
+
+	
 }
--- src/main/resources/static/prod/chooeat/js/mall_js/mall2.js
+++ src/main/resources/static/prod/chooeat/js/mall_js/mall2.js
@@ -55,9 +55,7 @@ function display() {
 		.then(res => res.json())
 		.then(prodList => {
 			pagination.empty();
-			//重新计算分页数量
 			const totalPages = Math.ceil(prodList.length / itemsPerPage);
-			// 根据新的分页状态筛选要显示的数据
 			const startIndex = (currentPage - 1) * itemsPerPage;
 			const endIndex = startIndex + itemsPerPage;
 			const currentPageData = prodList.slice(startIndex, endIndex);
@@ -107,34 +105,31 @@ document.getElementById('search_button').addEventListener('click', function(even
 	const main = document.querySelector('main');
 	main.innerHTML = "";
 	let search_text = document.getElementById('search_text').value.trim();
-
 	if (search_text === "") {
 		event.preventDefault(); // 阻止預設的提交行為
 		// 重新渲染上一次的搜尋結果
-
 		pagination.empty();
 		//重新計算分頁數量
 		const totalPages = Math.ceil(lastSearchResults.length / itemsPerPage);
-
 		// 根據新的分頁狀態篩選要顯示的資料
 		const startIndex = (currentPage - 1) * itemsPerPage;
 		const endIndex = startIndex + itemsPerPage;
 		const currentPageData = lastSearchResults.slice(startIndex, endIndex);
 
-		for (let prod of lastSearchResults) {
+		for (let prod of currentPageData) {
 			const price = prod.prodPrice.toLocaleString();
 			const star = Math.floor(prod.prodCommentScore);
-				// 星星
-				let starHtml = '';
-				for (let i = 1; i <= 5; i++) {
-					if (i <= star) {
-						starHtml += `<span class="star -on" data-star="${i}"><i class="fas fa-star"></i></span>`;
-					} else {
-						starHtml += `<span class="star" data-star="${i}"><i class="fas fa-star"></i></span>`;
-					}
+			// 星星
+			let starHtml = '';
+			for (let i = 1; i <= 5; i++) {
+				if (i <= star) {
+					starHtml += `<span class="star -on" data-star="${i}"><i class="fas fa-star"></i></span>`;
+				} else {
+					starHtml += `<span class="star" data-star="${i}"><i class="fas fa-star"></i></span>`;
 				}
+			}
 
-				main.innerHTML += `
+			main.innerHTML += `
 			        <a href="mall_prod.html?id=${prod.prodId}" value="${prod.prodId}" name="id" data-product-id="${prod.prodId}" class="prodLink" target="_blank">
 			          <div href="" target="_blank" class="prod">
 			            <img src="./chooeat/images/mall_image/mall1.jpg" />
@@ -159,8 +154,9 @@ document.getElementById('search_button').addEventListener('click', function(even
 		.then(res => res.json())
 		.then(prodList => {
 			if (prodList.length === 0) {
-				console.log("未找到任何商品");
-			} else {
+					main.innerHTML += `<h2 style="margin-top:30px; text-align: center;">未找到任何餐廳。</h2>`;
+					console.log("未找到任何餐廳");
+				} else { 
 				pagination.empty();
 				//重新計算分頁數量
 				const totalPages = Math.ceil(lastSearchResults.length / itemsPerPage);
@@ -175,16 +171,16 @@ document.getElementById('search_button').addEventListener('click', function(even
 				for (let prod of currentPageData) {
 					const price = prod.prodPrice.toLocaleString();
 					const star = Math.floor(prod.prodCommentScore);
-				// 星星
-				let starHtml = '';
-				for (let i = 1; i <= 5; i++) {
-					if (i <= star) {
-						starHtml += `<span class="star -on" data-star="${i}"><i class="fas fa-star"></i></span>`;
-					} else {
-						starHtml += `<span class="star" data-star="${i}"><i class="fas fa-star"></i></span>`;
+					// 星星
+					let starHtml = '';
+					for (let i = 1; i <= 5; i++) {
+						if (i <= star) {
+							starHtml += `<span class="star -on" data-star="${i}"><i class="fas fa-star"></i></span>`;
+						} else {
+							starHtml += `<span class="star" data-star="${i}"><i class="fas fa-star"></i></span>`;
+						}
 					}
-				}
-				main.innerHTML += `
+					main.innerHTML += `
 			        <a href="mall_prod.html?id=${prod.prodId}" value="${prod.prodId}" name="id" data-product-id="${prod.prodId}" class="prodLink" target="_blank">
 			          <div href="" target="_blank" class="prod">
 			            <img src="./chooeat/images/mall_image/mall1.jpg" />
@@ -203,7 +199,7 @@ document.getElementById('search_button').addEventListener('click', function(even
 		});
 });
 
-//=============================排序==============================
+// ============================= 排序 ==============================
 const sortSelect = document.getElementById('sort-select');
 sortSelect.addEventListener('change', () => {
 	const main = document.querySelector('main');
@@ -213,20 +209,25 @@ sortSelect.addEventListener('change', () => {
 	fetch(url)
 		.then(response => response.json())
 		.then(prodList => {
+			const searchKeyword = getSearchKeyword(); 
+            let filteredProdList = prodList;
+            if (searchKeyword) {
+                // 過濾包含搜尋關鍵字的商品
+                filteredProdList = prodList.filter(prod => prod.prodName.includes(searchKeyword));
+            }
 			// 處理獲取的商品資料
-			pagination.empty();
-			//重新計算分頁數量
-			const totalPages = Math.ceil(lastSearchResults.length / itemsPerPage);
+            pagination.empty();
+            //重新計算分頁數量
+            const totalPages = Math.ceil(filteredProdList.length / itemsPerPage);
+            // 根據新的分頁狀態篩選要顯示的資料
+            const startIndex = (currentPage - 1) * itemsPerPage;
+            const endIndex = startIndex + itemsPerPage;
+            const currentPageData = filteredProdList.slice(startIndex, endIndex);
 
-			// 根據新的分頁狀態篩選要顯示的資料
-			const startIndex = (currentPage - 1) * itemsPerPage;
-			const endIndex = startIndex + itemsPerPage;
-			const currentPageData = prodList.slice(startIndex, endIndex);
-
-			lastSearchResults = prodList; // 保存本次搜索結果
+			lastSearchResults = currentPageData; // 保存本次搜索結果
 			for (let prod of currentPageData) {
 				const price = prod.prodPrice.toLocaleString();
-			const star = Math.floor(prod.prodCommentScore);
+				const star = Math.floor(prod.prodCommentScore);
 				// 星星
 				let starHtml = '';
 				for (let i = 1; i <= 5; i++) {
@@ -257,3 +258,134 @@ sortSelect.addEventListener('change', () => {
 			console.error('發生錯誤:', error);
 		});
 });
+
+//===================================================================
+// 餐廳分類按鈕的事件監聽器
+const categoryButtons = document.querySelectorAll('.categoryDetail');
+const hotCategoryButtons = document.querySelectorAll('.hotCategory');
+hotCategoryButtons.forEach(button => {
+	button.addEventListener('click', function(event) {
+		const category = this.textContent;
+		// 清空主要內容
+		console.log(category);
+		const main = document.querySelector('main');
+		main.innerHTML = "";
+		// 發送餐廳分類請求
+		const url = "Prod/getall?category=" + category + "&action=getCategory";
+		fetch(url)
+			.then(res => res.json())
+			.then(prodList => {
+				if (prodList.length === 0) {
+					main.innerHTML += `<h2 style="margin-top:30px; text-align: center;">未找到任何餐廳。</h2>`;
+					console.log("未找到任何餐廳");
+				} else {
+					// 處理獲取的商品資料
+					pagination.empty();
+					//重新計算分頁數量
+					const totalPages = Math.ceil(lastSearchResults.length / itemsPerPage);
+					const startIndex = (currentPage - 1) * itemsPerPage;
+					const endIndex = startIndex + itemsPerPage;
+					const currentPageData = prodList.slice(startIndex, endIndex);
+
+					lastSearchResults = prodList; // 保存本次搜索結果
+					for (let prod of currentPageData) {
+						const price = prod.prodPrice.toLocaleString();
+						const star = Math.floor(prod.prodCommentScore);
+						// 星星
+						let starHtml = '';
+						for (let i = 1; i <= 5; i++) {
+							if (i <= star) {
+								starHtml += `<span class="star -on" data-star="${i}"><i class="fas fa-star"></i></span>`;
+							} else {
+								starHtml += `<span class="star" data-star="${i}"><i class="fas fa-star"></i></span>`;
+							}
+						}
+
+						main.innerHTML += `
+			        <a href="mall_prod.html?id=${prod.prodId}" value="${prod.prodId}" name="id" data-product-id="${prod.prodId}" class="prodLink" target="_blank">
+			          <div href="" target="_blank" class="prod">
+			            <img src="./chooeat/images/mall_image/mall1.jpg" />
+			            <span class="prodTitle">${prod.resName} | ${prod.prodName}</span>
+			            <div class="type" style="font-size: 18px;">${prod.resType}</div>
+			            <div class="add" style="font-size: 18px; margin-top: 5px; margin-bottom: 20px;">${prod.resAdd}</div>
+			            <span style="font-size: 20px;">NT$${price}</span>
+			            <span class="star_block" style="font-size: 20px;">
+			              ${starHtml}
+			            </span>
+			          </div>
+			        </a>`;
+					}
+					updatePagination(totalPages);
+				}
+			});
+	});
+
+})
+
+categoryButtons.forEach(button => {
+	button.addEventListener('click', function(event) {
+		const category = this.textContent;
+		// 清空主要內容
+		console.log(category);
+		const main = document.querySelector('main');
+		main.innerHTML = "";
+		// 發送餐廳分類請求
+		const url = "Prod/getall?category=" + category + "&action=getCategory";
+		fetch(url)
+			.then(res => res.json())
+			.then(prodList => {
+				if (prodList.length === 0) {
+					main.innerHTML += `<h2 style="margin-top:30px; text-align: center;">未找到任何餐廳。</h2>`;
+					console.log("未找到任何餐廳");
+				} else {
+					// 處理獲取的商品資料
+					pagination.empty();
+					//重新計算分頁數量
+					const totalPages = Math.ceil(lastSearchResults.length / itemsPerPage);
+					const startIndex = (currentPage - 1) * itemsPerPage;
+					const endIndex = startIndex + itemsPerPage;
+					const currentPageData = prodList.slice(startIndex, endIndex);
+
+					lastSearchResults = prodList; // 保存本次搜索結果
+					for (let prod of currentPageData) {
+						const price = prod.prodPrice.toLocaleString();
+						const star = Math.floor(prod.prodCommentScore);
+						// 星星
+						let starHtml = '';
+						for (let i = 1; i <= 5; i++) {
+							if (i <= star) {
+								starHtml += `<span class="star -on" data-star="${i}"><i class="fas fa-star"></i></span>`;
+							} else {
+								starHtml += `<span class="star" data-star="${i}"><i class="fas fa-star"></i></span>`;
+							}
+						}
+
+						main.innerHTML += `
+			        <a href="mall_prod.html?id=${prod.prodId}" value="${prod.prodId}" name="id" data-product-id="${prod.prodId}" class="prodLink" target="_blank">
+			          <div href="" target="_blank" class="prod">
+			            <img src="./chooeat/images/mall_image/mall1.jpg" />
+			            <span class="prodTitle">${prod.resName} | ${prod.prodName}</span>
+			            <div class="type" style="font-size: 18px;">${prod.resType}</div>
+			            <div class="add" style="font-size: 18px; margin-top: 5px; margin-bottom: 20px;">${prod.resAdd}</div>
+			            <span style="font-size: 20px;">NT$${price}</span>
+			            <span class="star_block" style="font-size: 20px;">
+			              ${starHtml}
+			            </span>
+			          </div>
+			        </a>`;
+					}
+					updatePagination(totalPages);
+				}
+			});
+	});
+
+})
+
+function getSearchKeyword() {
+    const searchInput = document.getElementById('search_text');
+    if (searchInput) {
+//		console.log(searchInput);
+        return searchInput.value.trim();
+    }
+    return null; 
+}
