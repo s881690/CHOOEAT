cfe13645da6c5194a1de637e85b559054fda0b19
YangYang <2bitlatch@gmail.com>
2023-06-07 16:20:47 +0800

    提升效能 關閉連線池資源

--- src/main/java/chooeat/account/daoimpl/AccountDaoImpl.java
+++ src/main/java/chooeat/account/daoimpl/AccountDaoImpl.java
@@ -90,18 +90,16 @@ public class AccountDaoImpl implements AccountDao {
 				accountVO.setAcc_phone(rs.getString("acc_phone"));
 				accountVO.setAcc_mail(rs.getString("acc_mail"));
 				accountVO.setAcc_add1(rs.getString("acc_add1"));
-				accountVO.setAcc_add2(rs.getString("acc_add2"));
-				accountVO.setAcc_add3(rs.getString("acc_add3"));
 				accountVO.setAcc_birth(rs.getDate("acc_birth"));
 				accountVO.setAcc_gender(rs.getInt("acc_gender"));
 				accountVO.setAcc_pic(rs.getBytes("acc_pic"));
 				accountVO.setAcc_text(rs.getString("acc_text"));
-				accountVO.setAcc_timestamp(rs.getTimestamp("acc_timestamp"));
-				accountVO.setAcc_state(rs.getInt("acc_state"));
-				accountVO.setOnline_status(rs.getInt("online_status"));
+				
 				return accountVO;
 			}
-
+			rs.close();
+			stmt.close();
+			conn.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
@@ -154,6 +152,9 @@ public class AccountDaoImpl implements AccountDao {
 
 				return result;
 			}
+		
+			stmt.close();
+			conn.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -191,6 +192,9 @@ public class AccountDaoImpl implements AccountDao {
 					result.add(account); // 將更新成功的帳戶加入結果列表
 				}
 			}
+			
+			stmt.close();
+			conn.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -284,6 +288,9 @@ public class AccountDaoImpl implements AccountDao {
 				result = 1;
 				
 			};
+		
+			stmt.close();
+			conn.close();
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -309,7 +316,9 @@ public class AccountDaoImpl implements AccountDao {
 				result = 1;
 				
 			};
-
+			
+			stmt.close();
+			conn.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
