423ad5a442a20159ce541786c7b939889d3d1362
sandy <s88169019@gmail.com>
2023-06-08 09:21:29 +0800

    預約頁檢查登入資訊

--- src/main/resources/static/reservation/js/reservation_js/checkLogin.js
+++ src/main/resources/static/reservation/js/reservation_js/checkLogin.js
@@ -6,12 +6,3 @@ window.addEventListener('DOMContentLoaded', function() {
     window.location.href = "http://localhost:8080/account/login.html";
   }
 });
-
-// 檢查頁面載入時的sessionStorage
-window.addEventListener('DOMContentLoaded', function() {
-  // 檢查sessionStorage中是否存在loginReq鍵
-  if (!sessionStorage.getItem('searchResult')) {
-    alert("請先搜尋想吃的餐廳~");
-    window.location.href = "http://localhost:8080/restaurant/Homepage.html";
-  }
-});
